8d0e718a2eb31101c3b12e619285a3ad1df7784e
ThisUsernameIsCool <60085040+ThisUsernameIsCool@users.noreply.github.com>
2022-06-05 19:02:22 -0400

    Major changes lmao

---
+++ script.js
@@ -0,0 +1,10 @@
+$.getJSON("https://cat-fact.herokuapp.com/facts", function(data) {
+  for (let i = 0; i < data.length; i++) {
+    var tag = document.createElement("p"); // <p></p>
+    var text = document.createTextNode(data[i]["text"]);
+    tag.appendChild(text); // <p>TEST TEXT</p>
+    var element = document.getElementsByTagName("body")[0];
+    element.appendChild(tag);
+  }
+  document.append(facts);
+});
