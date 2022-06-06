10eff6d32bb3dac086c05f9f6faceef0436a0ad1
NumberEater <reddickgrant@gmail.com>
2022-06-06 01:10:37 -0400

    a

--- old/index.php
+++ old/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<?php
-		if($_POST["username"] != "NumberEater" || $_POST["password"] != "4012Ginger!") {
+		if($_POST["username"] != "deezmaster" || $_POST["password"] != "balls") {
 			header("Location: ../index.html");
 			die();
 		}
@@ -20,7 +20,10 @@
 		</div>
 		<div class="info">
 			<p>This is my website. If you're here that means you have the username and password.</p>
+			<p>The website is hosted on a rasbperry pi zero with 512 mb of ram.</p>
 		</div>
+		<h2>Image of a toaster</h1>
+		<img alt="toaster" href="https://solis.com/media/catalog/product/9/2/920-11_cmyk-007_toastersteel-01460.jpg?optimize=medium&fit=bounds&height=700&width=700" />
 	</center>
 </body>
 </html>
